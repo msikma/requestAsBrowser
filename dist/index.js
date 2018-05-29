0fd6f86422593b6b56a3a485b9b605471b023c70
Michiel Sikma <michiel@sikma.org>
2018-05-29 22:55:16 +0200

    Build our really big hack for Pixiv

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,10 @@ var _fileCookieStore2 = _interopRequireDefault(_fileCookieStore);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 // These headers are sent with each request to make us look more like a real browser.
 var browserHeaders = {
   'Accept-Language': 'en-US,en;q=0.9,ja;q=0.8,nl;q=0.7,de;q=0.6,es;q=0.5,it;q=0.4,pt;q=0.3',
@@ -111,18 +115,90 @@ var requestAsBrowser = function requestAsBrowser(url, cookieJar) {
  * after the file has been fully saved. A pipe is used to write the file,
  * meaning that the file will be gradually filled with data, and on premature exit
  * the file will have partial data.
+ *
+ * mightBe: Special hack for Pixiv: a file might be a jpg, or it might be a png.
+ * This is the least expensive way to check when downloading a lot of files.
  */
 var downloadFileAsBrowser = exports.downloadFileAsBrowser = function downloadFileAsBrowser(url, name, cookieJar) {
   var extraHeaders = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : {};
   var gzip = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : true;
   var reqOverrides = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : {};
+  var mightBeURL = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : null;
+  var mightBeName = arguments.length > 7 && arguments[7] !== undefined ? arguments[7] : null;
+  return new Promise(function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(resolve, reject) {
+      var args, main, secondary;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              args = _extends({ headers: _extends({}, browserHeaders, extraHeaders), jar: cookieJar, gzip: gzip }, reqOverrides);
+              _context.next = 3;
+              return requestAsBrowser.apply(undefined, [url].concat(_toConsumableArray(args)));
+
+            case 3:
+              main = _context.sent;
+
+              if (!(main.response && main.response.statusCode === 200)) {
+                _context.next = 8;
+                break;
+              }
+
+              _context.next = 7;
+              return saveBinaryFile(main.body, name);
+
+            case 7:
+              return _context.abrupt('return', resolve({ response: main.response, body: main.body }));
+
+            case 8:
+              if (!(main.response && main.response.statusCode === 404 && mightBeURL)) {
+                _context.next = 17;
+                break;
+              }
+
+              _context.next = 11;
+              return requestAsBrowser.apply(undefined, [mightBeURL].concat(_toConsumableArray(args)));
+
+            case 11:
+              secondary = _context.sent;
+
+              if (!(secondary.response && secondary.response.statusCode === 200)) {
+                _context.next = 16;
+                break;
+              }
+
+              _context.next = 15;
+              return saveBinaryFile(main.body, mightBeName);
+
+            case 15:
+              return _context.abrupt('return', resolve({ response: secondary.response, body: secondary.body }));
+
+            case 16:
+              return _context.abrupt('return', reject(secondary));
+
+            case 17:
+              return _context.abrupt('return', reject(main));
+
+            case 18:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, undefined);
+    }));
+
+    return function (_x12, _x13) {
+      return _ref.apply(this, arguments);
+    };
+  }());
+};
+
+/**
+ * Saves binary data to a destination file.
+ */
+var saveBinaryFile = function saveBinaryFile(data, dest) {
   return new Promise(function (resolve, reject) {
-    (0, _request2.default)(_extends({
-      url: url,
-      headers: _extends({}, browserHeaders, extraHeaders),
-      jar: cookieJar,
-      gzip: gzip
-    }, reqOverrides), reqCallback(resolve, reject)).pipe(_fs2.default.createWriteStream(name));
+    _fs2.default.writeFile(dest, data, { encoding: null });
   });
 };
 
